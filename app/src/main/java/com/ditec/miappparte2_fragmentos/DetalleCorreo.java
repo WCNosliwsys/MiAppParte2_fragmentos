c31cfd3efb1ed4ae1cbe99a8f3ab7d31a5558bfe
Wilson Callisaya <demarktec@gmail.com>
2020-10-09 22:38:46 -0500

    Recibiendo parametros del adaptador en detallecorreo

--- app/src/main/java/com/ditec/miappparte2_fragmentos/DetalleCorreo.java
+++ app/src/main/java/com/ditec/miappparte2_fragmentos/DetalleCorreo.java
@@ -3,12 +3,18 @@ package com.ditec.miappparte2_fragmentos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetalleCorreo extends AppCompatActivity {
 
+    TextView midetalle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_correo);
+        midetalle=findViewById(R.id.TxtDetalle);
+        midetalle.setText("De: "+getIntent().getExtras().getString("de")+
+                " Asunto: "+getIntent().getExtras().getString("asunto")+
+                " Contenido: "+getIntent().getExtras().getString("texto"));
     }
 }
